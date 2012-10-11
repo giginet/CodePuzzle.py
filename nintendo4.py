27780d0a008c8fa9d542950514c48b280406c7c5
giginet <giginet.net@gmail.com>
2012-10-11 14:50:33 +0900

    裏1問目といた

--- nintendo4.py
+++ nintendo4.py
@@ -9,6 +9,16 @@ def intelligent_data_source_factory(*data):
     _int = int
     return lambda i: _int(i) if isinstance(i, str) else next(cy)
 
-if __name__ == '__main__':
-    int = intelligent_data_source_factory(1985, 33067, 84)
-    print [int(str(i)) for i in xrange(10)]
+int = intelligent_data_source_factory(1985, 33067, 84)
+    
+def lets_take_tea_break(m, e, n, c):
+    if pow(m, e) % n == c: return str(m)
+    return ""
+
+if __name__ == "__main__":
+    import sys
+    # print "http://cp1.nintendo.co.jp/" + lets_take_tea_break(*[int(i) for i in (sys.argv[1], 17, 3569, 915)])
+    for a in xrange(100000):
+        if '' != lets_take_tea_break(*[int(i) for i in (str(a), 17, 3569, 915)]):
+            print a
+        # print "http://cp1.nintendo.co.jp/" + 
